1b55a846409691d56a69820df944edc1e3822c9a
Karnydal <97188586+Karnydal@users.noreply.github.com>
2022-06-12 00:14:11 +0300

    major improvement

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,37 +1,38 @@
 // fixed according to lection
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-let data = {
-    email: '',
-    message: '',
+const formRef = document.querySelector('.feedback-form');
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+if (localStorage.getItem('feedback-form-state')) {
+  formData = JSON.parse(localStorage.getItem('feedback-form-state'));
 }
 
-if (localStorage.getItem("feedback-form-state")) {
-    data = JSON.parse(localStorage.getItem("feedback-form-state"));
-}
+formRef.querySelector('[name="email"]').value = formData.email;
+formRef.querySelector('[name="message"]').value = formData.message;
 
-form.querySelector('[name="email"]').value = data.email;
-form.querySelector('[name="message"]').value = data.message;
-
-form.addEventListener('input', throttle(onFormInput, 500));
-form.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', throttle(onFormInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
 
 function onFormInput(e) {
-    data[e.target.name] = e.target.value;
-    localStorage.setItem("feedback-form-state", JSON.stringify(data));
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
 function onFormSubmit(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
 
-    localStorage.clear();
-    e.target.reset();
-    data.email = '';
-    data.message = '';
+  localStorage.clear();
+  e.target.reset();
+  formData.email = '';
+  formData.message = '';
 }
 
-// console.log(formData);
+console.log(formData);
 
